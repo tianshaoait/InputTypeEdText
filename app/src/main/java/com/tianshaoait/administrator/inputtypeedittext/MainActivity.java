bb0dcc82097817deb857c9512c4dbb51c1602c4f
tianshaoait <31428233+tianshaoait@users.noreply.github.com>
2017-08-29 17:20:45 +0800

    InputTypeEdText
    InputTypeEdText

---
+++ app/src/main/java/com/tianshaoait/administrator/inputtypeedittext/MainActivity.java
@@ -0,0 +1,20 @@
+package com.tianshaoait.administrator.inputtypeedittext;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+//    InputType editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        inView();
+    }
+
+    private void inView() {
+//        editText = (InputType) findViewById(R.id.activity_main_edtx);
+    }
+}
